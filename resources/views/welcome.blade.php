641ab15f873179b48b33ee89b5e3cfb6f5d44866
Kelj-menc <keljmenc@gmail.com>
2020-09-08 18:27:57 +0200

    front page update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    IDE GAAAAASSSSSSS!!!!!
                 </div>
 
                 <div class="links">
